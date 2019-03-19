02565c8b18ba88ed8f940665cc76004b0c739747
Luca Pipolo <luca@netzstrategen.com>
2019-03-19 11:47:10 +0100

    Fixed image disclaimer was not shown on lightbox images.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,5 +9,10 @@
       }, 600);
     });
 
+    if (typeof $.fn.lightGallery === 'function' && $('body').hasClass('woocommerce') && $('.images').length > 0) {
+      $('.images').on('onAfterAppendSubHtml.lg', function (event, index) {
+        $('.lg-sub-html').append('<span>Bild zeigt evtl. Zubehör. Lieferumfang beachten!</span>');
+      });
+    }
   });
 })(jQuery);
--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Image Disclaimer
-  Version: 1.1.2
+  Version: 1.2.0
   Text Domain: image-disclaimer
   Description: Display a text (e.g. a disclaimer) on WooCommerce product images.
   Author: netzstrategen
