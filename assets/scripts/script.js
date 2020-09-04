f07f22319a9c8ac21f8f5e36391b73e19901ed6a
Joan López <joan@netzstrategen.com>
2020-09-04 10:49:59 +0200

    Added support for fancybox library. (#5)

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,23 @@
 (function ($) {
   $(document).ready(function () {
-    if (typeof $.fn.lightGallery === 'function' && $('body').hasClass('woocommerce') && $('.images').length > 0) {
-      $('.images').on('onAfterAppendSubHtml.lg', function (event, index) {
-        $('.lg-sub-html').append('<span>Bild zeigt evtl. Zubehör. Lieferumfang beachten!</span>');
-      });
+    if ($('body').hasClass('woocommerce') && $('.images').length > 0) {
+      var disclaimer = 'Bild zeigt evtl. Zubehör. Lieferumfang beachten!';
+
+      // Support for lightgallery.
+      if (typeof $.fn.lightGallery === 'function') {
+        $('.images').on('onAfterAppendSubHtml.lg', function (event, index) {
+          $('.lg-sub-html').append('<span>' + disclaimer + '</span>');
+        });
+      }
+
+      // Support for fancybox.
+      if (typeof $.fn.fancybox === 'function' && $('body').hasClass('woocommerce') && $('.images').length > 0) {
+        $(document).on('beforeShow.fb', function(e, instance, slide) {
+          if (!$('.fancybox-caption__image-disclaimer').length) {
+            $('.fancybox-caption').append('<div class="fancybox-caption__image-disclaimer">' + disclaimer + '</div>');
+          }
+        });
+      }
     }
   });
 })(jQuery);
